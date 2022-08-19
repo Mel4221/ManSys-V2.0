7521d0bf617b827036966609e369b137ba2e4f69
Mel4221 <melqui4251@gmail.com>
2022-08-19 16:39:01 -0400

    Susto

--- SISCANE/Properties/AssemblyInfo.cs
+++ SISCANE/Properties/AssemblyInfo.cs
@@ -6,19 +6,19 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("SISCANE")]
+[assembly: AssemblyTitle("ManSys")]
 [assembly: AssemblyDescription("Programa de Manejo de Nomina Salarial")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LaiaSoluctions(C)")]
-[assembly: AssemblyProduct("SISCANE")]
+[assembly: AssemblyCompany("MBV")]
+[assembly: AssemblyProduct("ManSys")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("MIT")]
 [assembly: AssemblyCulture("")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
 // para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
 // COM, establezca el atributo ComVisible en true en este tipo.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
 [assembly: Guid("b96ef594-d24e-4edc-8c73-98dff012c2ee")]
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("es-DO")]
+[assembly: NeutralResourcesLanguage("")]
 
