c44357cd859d95e231efbcc94d7d1038277d4f0b
Mel4221 <melqui4251@gmail.com>
2022-08-21 14:05:37 -0400

    Aqui ya se encuentra una version  con la API  de el connection string por seguridad

--- DataAccess/ConexionaSql.cs
+++ DataAccess/ConexionaSql.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net; 
 using System.Data.SqlClient;
 
 namespace DataAccess
@@ -11,7 +13,34 @@ namespace DataAccess
     {
         private readonly string connectionString;
         public ConexionaSql() {
-            connectionString = "integrated security=SSPI;data source=DESKTOP-802OK33;" + "persist security info=False;initial catalog=SISCANE";
+            string connection = null;
+           
+            string link = String.Format($"http://mbvapps.xyz/app/api/Worker?user={Environment.UserName}&token=F%5E)Enurc)%5E0A%26%2B)aeW5%3F%26!(!1%2Bm(1%3FnZ");
+
+
+
+            WebRequest request = WebRequest.Create(link);
+            request.Method = "GET";
+            //  request.Headers["Token"] = "123-456-789";
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            HttpWebResponse responceObj = null;
+
+            responceObj = (HttpWebResponse)request.GetResponse();
+            request.Timeout = 1;
+            string result = null;
+            using (Stream stream = responceObj.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream);
+                result = reader.ReadToEnd();
+            }
+            // this is  a quick fix because is comming as a json format
+            // and i just need the string 
+
+            connection = result.Substring(2, result.Length - 4);
+            connectionString = connection; 
+
+
         }
         protected SqlConnection GetConnection(){
             return new SqlConnection(connectionString);
